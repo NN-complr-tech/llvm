fd98e176f54f4574b05ff1e2aef69acbbb4fd887
fartekool <levonychev2006@gmail.com>
2026-03-11 22:35:21 +0300

    first ver

---
+++ clang/test/compiler-course/levonychev-i-lab1-test/test.cpp
@@ -0,0 +1,35 @@
+// RUN: %clang_cc1 -load %llvmshlibdir/levonychev-i-lab1_ClangAST%pluginext -plugin const_change_plugin -fsyntax-only %s 2>&1 | FileCheck %s
+
+
+
+// CHECK: void test_func1(const float{{\*}} const x1) {
+
+void test_func1(float* x1) {
+    float var = *x1;
+}
+
+
+// CHECK: const int{{\*}} const x2_ptr = &x2;
+void test_func2() {
+    int x2 = 0;
+    int* x2_ptr = &x2;
+}
+
+
+
+// CHECK: int{{\*}} const x3_ptr = &x3;
+void test_func3() {
+    int x3 = 0;
+    int* x3_ptr = &x3;
+    *x3_ptr = 1;
+}
+
+// CHECK-NOT: int{{\*}} const x4_ptr = &x4;
+void test_func4() {
+    int x4 = 0;
+    int* x4_ptr = &x4;
+    x4_ptr++;
+    x4_ptr--;
+    *x4_ptr = 1;
+}
+
