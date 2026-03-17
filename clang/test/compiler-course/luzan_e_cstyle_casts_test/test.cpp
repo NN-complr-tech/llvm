aacd292433bd8cd95ee5221af2526697e93f0086
smallAbyss <eal1105@yandex.ru>
2026-03-17 18:38:05 +0300

    add tests

--- clang/test/compiler-course/luzan_e_cstyle_casts_test/test.cpp
+++ clang/test/compiler-course/luzan_e_cstyle_casts_test/test.cpp
@@ -4,27 +4,27 @@
 int foo(int a) {
     return 0;
 }
-int StaticTest0(float a) {
+int staticTest0(float a) {
     return (int)a; 
-    // CHECK-LABEL: int StaticTest0(float a)
+    // CHECK-LABEL: int staticTest0(float a)
     // CHECK-NEXT: return static_cast<int>(a)
 }
 
-int StaticTest1(float a) {
+int staticTest1(float a) {
     return (int)a;
-    // CHECK-LABEL: int StaticTest1(float a)
+    // CHECK-LABEL: int staticTest1(float a)
     // CHECK-NEXT: return static_cast<int>(a);
 }
 
-double StaticTest2(float a) {
+double staticTest2(float a) {
     return (double)a;
-    // CHECK-LABEL: double StaticTest2(float a)
+    // CHECK-LABEL: double staticTest2(float a)
     // CHECK-NEXT: return static_cast<double>(a);
 }
 
-int StaticExpressionTest(float a, float b) {
+int staticExpressionTest(float a, float b) {
     return (int)(a + b);
-    // CHECK-LABEL: int StaticExpressionTest(float a, float b)
+    // CHECK-LABEL: int staticExpressionTest(float a, float b)
     // CHECK-NEXT: return static_cast<int>((a + b));
 }
 
@@ -40,15 +40,15 @@ int staticCastTernary(float a, float b) {
     // CHECK-NEXT: return static_cast<int>((a > 0 ? a : b));
 }
 
-int NestedCasts(float a) {
+int nestedCasts(float a) {
     return (int)(double)a;
-    // CHECK-LABEL: int NestedCasts(float a)
+    // CHECK-LABEL: int nestedCasts(float a)
     // CHECK-NEXT: static_cast<int>(static_cast<double>(a))
 }
 
-int NestedCasdts(float a, double b) {
+int complexNestedCasts(float a, double b) {
     return (int)((double)a + b);
-    // CHECK-LABEL: int NestedCasdts(float a, double b)
+    // CHECK-LABEL: int complexNestedCasts(float a, double b)
     // CHECK-NEXT: static_cast<int>((static_cast<double>(a) + b))
 }
 
@@ -64,4 +64,28 @@ void reinterpretCase(void* p) {
     int* x = (int*)p;
     // CHECK-LABEL: void reinterpretCase(void* p)
     // CHECK-NEXT: int* x = reinterpret_cast<int *>(p)
+}
+
+int noCast(int a) {
+    return a;
+    // CHECK-LABEL: int noCast2(int a)
+    // CHECK: return a;
+}
+
+int alreadyCpp(float a) {
+    return static_cast<int>(a);
+    // CHECK-LABEL: int alreadyCpp(float a)
+    // CHECK: return static_cast<int>(a);
+}
+
+void sameLine(float a, float b) {
+    int x = (int)a, y = (int)b;
+    // CHECK-LABEL: void sameLine(float a, float b)
+    // CHECK: int x = static_cast<int>(a), y = static_cast<int>(b);
+}
+
+void manyCasts(float a, float b) {
+    int x = (int)a + (int)b;
+    // CHECK-LABEL: void sameLine(float a, float b)
+    // CHECK: int x = static_cast<int>(a) + static_cast<int>(b);
 }
