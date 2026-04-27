edda2bed2fa31b85e072ef685e4f8ee69262687f
fartekool <levonychev2006@gmail.com>
2026-04-27 17:41:18 +0300

    first ver

--- llvm/compiler-course/backend/levonychev-i-lab3/NullPtrPass.cpp
+++ llvm/compiler-course/backend/levonychev-i-lab3/NullPtrPass.cpp
@@ -3,23 +3,51 @@
 #include "X86Subtarget.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/TargetRegisterInfo.h"
 
 using namespace llvm;
 
 namespace {
-class NullPtrDereferencePass : public MachineFunctionPass {
+class CheckNullPtrPass : public MachineFunctionPass {
 public:
   static char ID;
-  NullPtrDereferencePass() : MachineFunctionPass(ID) {}
-  bool runOnMachineFunction(MachineFunction &MF) override;
+  CheckNullPtrPass() : MachineFunctionPass(ID) {}
+
+  bool runOnMachineFunction(MachineFunction &MF) override {
+    bool Changed = false;
+    const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
+    
+    for (auto &MBB : MF) {
+      for (auto MI = MBB.begin(); MI != MBB.end(); ++MI) {
+        if (MI->mayLoad() || MI->mayStore()) {
+          int MemOpNo = X86II::getMemoryOperandNo(MI->getDesc().TSFlags);
+          if (MemOpNo == -1) continue;
+
+          const MachineOperand &BaseRegOp = MI->getOperand(MemOpNo + X86::AddrBaseReg);
+          
+          if (!BaseRegOp.isReg() || !BaseRegOp.getReg().isValid()) continue;
+
+          Register PtrReg = BaseRegOp.getReg();
+
+          if (PtrReg == X86::RSP || PtrReg == X86::RBP || PtrReg == X86::RIP) continue;
+
+          DebugLoc DL = MI->getDebugLoc();
+          BuildMI(MBB, MI, DL, TII->get(X86::MOV64rr), X86::RDI)
+              .addReg(PtrReg);
+          auto MIB = BuildMI(MBB, MI, DL, TII->get(X86::CALL64pcrel32))
+                        .addExternalSymbol("check_null");
+          
+          MIB.addReg(X86::RDI, RegState::Implicit);
+
+          Changed = true;
+        }
+      }
+    }
+    return Changed;
+  }
 };
 
-char NullPtrDereferencePass::ID = 0;
-
-bool NullPtrDereferencePass::runOnMachineFunction(MachineFunction &func) {
-  
-}
+char CheckNullPtrPass::ID = 0;
 } // namespace
 
-static RegisterPass<NullPtrDereferencePass> X("levonychev-nullptr-x86", "description pass", false,
-                                   false);
+static RegisterPass<CheckNullPtrPass> X("levonychev-nullptr-x86", "Call check_null", false, false);
